c0dc58afb390808cc9894f443bcd2bb14f60a4ae
EssamN15 <100591179+EssamN15@users.noreply.github.com>
2023-02-13 15:22:29 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,12 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Menu')
+streamlit.header('Breakfast Menu')
 
-streamlit.text('Beans and Egg')
-streamlit.text('Pancakes')
+streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
+streamlit.text(🐔'Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')   
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
